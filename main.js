e276ef39facd669e9e741be7b864d798f17e30e9
zakitripamungkas03 <zakitripamungkas03@gmail.com>
2024-06-04 14:39:02 +0000

    update 1.2

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Slider functionality
+  const prevButton = document.querySelector('.prev-button');
+  const nextButton = document.querySelector('.next-button');
+  const slider = document.querySelector('.image-slider');
+  const slides = document.querySelectorAll('.slider-image');
+  let currentIndex = 0;
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const prevButton = document.querySelector('.prev-button');
-    const nextButton = document.querySelector('.next-button');
-    const slider = document.querySelector('.image-slider');
-    const slides = document.querySelectorAll('.slider-image');
-    let currentIndex = 0;
+  // Set the width of the slider to match the number of slides
+  slider.style.width = `${slides.length * 100}%`;
+  slides.forEach(slide => {
+    slide.style.width = `${100 / slides.length}%`;
+  });
 
-    // Set the width of the slider to match the number of slides
-    slider.style.width = `${slides.length * 100}%`;
-    slides.forEach(slide => {
-      slide.style.width = `${100 / slides.length}%`;
-    });
-
-    prevButton.addEventListener('click', () => {
-      if (currentIndex > 0) {
-        currentIndex--;
-        updateSliderPosition();
-      }
-    });
-
-    nextButton.addEventListener('click', () => {
-      if (currentIndex < slides.length - 1) {
-        currentIndex++;
-        updateSliderPosition();
-      }
-    });
-
-    function updateSliderPosition() {
-      slider.style.transform = `translateX(-${currentIndex * 100 / slides.length}%)`;
+  prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateSliderPosition();
     }
   });
 
+  nextButton.addEventListener('click', () => {
+    if (currentIndex < slides.length - 1) {
+      currentIndex++;
+      updateSliderPosition();
+    }
+  });
+
+  function updateSliderPosition() {
+    slider.style.transform = `translateX(-${currentIndex * 100 / slides.length}%)`;
+  }
+
+  // Burger menu functionality
+  const menuIcon = document.getElementById('menu-icon');
+  const navLinks = document.getElementById('nav-links');
+
+  menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+});
